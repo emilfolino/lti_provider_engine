701e9f2e14adeb4e39384354f52b93090b03df8b
Emil Folino <emilfolino@gmail.com>
2025-08-29 11:09:59 +0200

    Explicitly setting the coder for serialization

--- app/models/lti_provider/launch.rb
+++ app/models/lti_provider/launch.rb
@@ -6,7 +6,7 @@ module LtiProvider
 
     attr_accessor :lti_errormsg
 
-    serialize :provider_params, Hash
+    serialize :provider_params, coder: Hash
 
     def self.initialize_from_request(provider, request)
       launch = new
