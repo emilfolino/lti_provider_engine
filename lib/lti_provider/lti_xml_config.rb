519807759c0d1f91e9031c70cd219ad4c0dd4513
Emil Folino <emilfolino@gmail.com>
2025-08-29 11:01:58 +0200

    Added aliases: true to YAML::load in lti xml_config

--- lib/lti_provider/lti_xml_config.rb
+++ lib/lti_provider/lti_xml_config.rb
@@ -1,7 +1,7 @@
 module LtiProvider
   module LtiXmlConfig
     def self.load_config
-      YAML::load(File.open(config_file))[Rails.env]
+      YAML::load(File.open(config_file), aliases: true)[Rails.env]
     end
 
     def self.config_file
